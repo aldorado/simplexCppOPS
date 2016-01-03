93be708cd30eb1b9dd47f07f883934d813dfc0f4
Anuar Suarez <anuar.suarez@hotmail.com>
2016-01-03 13:43:06 +0100

    Schlechtes Interface hinzugefügt!

--- main.cpp
+++ main.cpp
@@ -4,20 +4,43 @@
 using namespace std;
 
 int main() {
+
     string pathtosimplex;
-    cout << "Geben Sie den Pfad zum Simplex-File an:" << endl;
-    cin >> pathtosimplex;
+    char eingabe = '#';
+    bool beenden = false;
 
-    Tableau t = filereader(pathtosimplex);
-    //Tableau t = filereader("F:\\simplextest");
+    while(!beenden) {
 
-    double* solution = lpsolve(t);
-    cout << endl << "LOESUNGSWERTE:" << endl;
+        cout << endl << "1) Simplexfile angeben" << endl;
+        cout << "2) --------------------" << endl;
+        cout << "x) Programm Beenden" << endl;
+        cout << "Eingabe: ";
 
-    for(int i = 0; i < t.getN(); ++i)
-        cout << "x" << i+1 << " = " << solution[i] << endl;
+        cin >> eingabe;
 
-    cout << "Optimaler ZF-Wert = " << solution[t.getN()] << endl;
+        cout << endl;
+
+        switch (eingabe) {
+            case '1':
+            {
+                cout << "Geben Sie den Pfad zum Simplex-File an:" << endl;
+                cin >> pathtosimplex;
+                Tableau t = filereader(pathtosimplex);
+                double* solution = lpsolve(t);
+                cout << endl << "LOESUNGSWERTE:" << endl;
+                for(int i = 0; i < t.getN(); ++i)
+                    cout << "x" << i+1 << " = " << solution[i] << endl;
+                cout << "Optimaler ZF-Wert = " << solution[t.getN()] << endl;
+            }
+                break;
+            case 'x':
+                beenden = true;
+                break;
+            default:
+                break;
+        }
+
+    }
 
     return 0;
 }
--- utilities.cpp
+++ utilities.cpp
@@ -6,51 +6,51 @@
 
 Tableau filereader(string pathToFile) {
 
-    try {
-
         ifstream file(pathToFile);
         // n - Variablen (Spalten) , k - Nebenbedingungen (Reihen)
-        int n,k;
-        file >> n >> k;
+        if(file) {
+            int n,k;
+            file >> n >> k;
 
-        // Array zum Speichern der Variablen
-        double* c = new double[n];
+            // Array zum Speichern der Variablen
+            double* c = new double[n];
 
-        for (int i = 0; i < n; ++i)
-            file >> c[i];
+            for (int i = 0; i < n; ++i)
+                file >> c[i];
 
 
-        //Array zum Speichern der Matrix, n+1 da wir eine zusaetzliche Spalte haben
-        double* b = new double[k];
-        double** matrice = new double*[k];
-        for (int i = 0; i < k; ++i) {
-            matrice[i] = new double[n];
+            //Array zum Speichern der Matrix, n+1 da wir eine zusaetzliche Spalte haben
+            double* b = new double[k];
+            double** matrice = new double*[k];
+            for (int i = 0; i < k; ++i) {
+                matrice[i] = new double[n];
 
-            for (int j = 0; j < n; ++j)
-                file >> matrice[i][j];
+                for (int j = 0; j < n; ++j)
+                    file >> matrice[i][j];
 
-            file >> b[i];
-        }
-
-        cout << n << " " << k << endl;
-
-        for (int i = 0; i < k; ++i) {
-            for (int j = 0; j < n; ++j) {
-                cout << " " << matrice[i][j];
+                file >> b[i];
             }
-            cout << " <= " << b[i];
-            cout << endl;
+
+            cout << n << " " << k << endl;
+
+            for (int i = 0; i < k; ++i) {
+                for (int j = 0; j < n; ++j) {
+                    cout << " " << matrice[i][j];
+                }
+                cout << " <= " << b[i];
+                cout << endl;
+            }
+
+            for (int l = 0; l < n; ++l)
+                cout << " " << c[l];
+
+            cout << endl << endl;
+
+            return Tableau(n , c, k, matrice, b);
+        }
+        else {
+            cout << "Filereader Exception" << endl;
+            exit (EXIT_FAILURE);
         }
 
-        for (int l = 0; l < n; ++l)
-            cout << " " << c[l];
-
-        cout << endl << endl;
-
-        return Tableau(n , c, k, matrice, b);
-
-    } catch (...) {
-        cout << "Filereader Exception" << endl;
-        exit (EXIT_FAILURE);
-    }
 }

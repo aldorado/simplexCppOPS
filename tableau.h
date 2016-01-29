7c2faae2200cd3fce874dd67cf56b9d0c444a9db
a1301624 <roman.karaba.jr@gmail.com>
2016-01-29 16:35:58 +0100

    main.cpp, simplex.cpp and tableau.h commented in detail (tableau.cpp in process)

--- tableau.h
+++ tableau.h
@@ -10,31 +10,29 @@ using namespace std;
 
 class Tableau {
 private:
-    double** tableauArray;
-    int tableauColumns;
+    double** tableauArray; // pointer for the 2D array in which we save the tableau
+    int tableauColumns; // we save the number of columns and rows seperately for the future ease of use while executing the algorithm
     int tableauRows;
-    bool optimum = false;
+    bool optimum = false; // the optimum variable describes the state of the computation, if the optimum is found the variable is set to true
 
-    int pivotColumn;
-    int pivotRow;
-    double pivotElement;
-
-    int* x;
+    int pivotColumn; // after we find the pivot using the findPivot() function, we save the pivot column and row
+    int pivotRow;   //  into these variables.
+    double pivotElement; // the pivot element is saved into this variable so we don't have to use the array index
 
     Tableau() {}
 
 public:
-    Tableau(int n, double* c, int k, double** A, double* b);
+    Tableau(int n, double* c, int k, double** A, double* b); // the custom constructor for our tableau class
     ~Tableau();
     int getN() { return tableauColumns - tableauRows; }
     int getK() { return tableauRows - 1; }
 
-    void findPivot();
-    void simplexStep();
-    bool checkOptimum();
-
-    double* solution();
-    void printTableau();
+    void findPivot();       // <-
+    void simplexStep();     // <-   a better description for these
+    bool checkOptimum();    // <-   functions is found in the
+                            //      cpp file, since these are only
+    double* solution();     // <-   function declarations
+    void printTableau();    // <
 };
 
 
